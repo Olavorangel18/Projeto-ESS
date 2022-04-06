280c83d84ca742daf41abdbe1f3fd6a097050533
Olavo Rangel <olavo.rangel@elogroup.com.br>
2022-04-06 12:42:36 -0300

    listagem-vagas: criação e estilização do componente

--- src/app/listagem-vagas/listagem-vagas.component.ts
+++ src/app/listagem-vagas/listagem-vagas.component.ts
@@ -10,6 +10,35 @@ export class ListagemVagasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(this.acharSwitch, 100);
   }
+
+  mudarSituacaoInscricao(e:any){
+      setTimeout(this.acharSwitch, 200);
+  }
+
+  acharSwitch(){
+    let listaVagasSwitchAtivados:HTMLElement[] = [];
+    let listaVagasSwitchInativos:HTMLElement[] = [];
+    document.querySelectorAll<HTMLElement>("li").forEach(
+      vaga => {
+        if(vaga.children[3].querySelector("igx-switch")?.classList[1]){
+          listaVagasSwitchAtivados.push(vaga)
+        }else{
+          listaVagasSwitchInativos.push(vaga)
+        }
+      }
+    )
+      listaVagasSwitchAtivados.forEach(vaga => {
+        vaga.children[0].innerHTML = "candidatado"
+        vaga.children[0].classList.add('candidatado')
+      })
+      listaVagasSwitchInativos.forEach(vaga => {
+        vaga.children[0].innerHTML = "não candidatado"
+        vaga.children[0].classList.remove('candidatado')
+      })
+  }
+
+
 
 }
