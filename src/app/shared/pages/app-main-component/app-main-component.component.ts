4511fefcd2c55fb23d5740f06501d8af5f071931
Olavo Rangel <olavo.rangel@elogroup.com.br>
2022-04-11 16:32:34 -0300

    app-main: refatoração melhorar legibilidade

--- src/app/shared/pages/app-main-component/app-main-component.component.ts
+++ src/app/shared/pages/app-main-component/app-main-component.component.ts
@@ -6,8 +6,6 @@ import { NotificacaoComponent } from 'src/app/notificacao/notificacao.component'
 import { userLogadoModel } from './../../../login/model/userLogado.model';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router} from '@angular/router';
-import { ViewChild } from '@angular/core';
-
 
 @Component({
   selector: 'app-app-main-component',
@@ -17,7 +15,6 @@ import { ViewChild } from '@angular/core';
 export class AppMainComponentComponent implements OnInit {
 
   constructor(private router: Router) { }
-
 
   tipoUsuario: userLogadoModel|undefined;
   @Output() listaSelecionadosEmitter: EventEmitter<any> = new EventEmitter();
@@ -37,97 +34,107 @@ export class AppMainComponentComponent implements OnInit {
   backgroundMenuNotificacao:string = "background-azul"
   strokeNotificacao:string = "white"
 
-
   ngOnInit(): void {
     this.pegarTipoUsuario();
     this.verificarExistenciaUsuarioLogado();
   }
 
+  //********************************************************
+  //                 Inicialização Menu
+  //********************************************************
+
   onActivate(e:any){
-    if(e instanceof ListagemVagasComponent){
-      this.backgroundMenuListagem = "background-branco";
-      this.strokeListagem = "#41B8D2";
-    }
-    else if(e instanceof MeuCadastroComponent){
-      this.backgroundMenuUser = "background-branco";
-      this.strokeUser="#41B8D2";
-    }
-    else if(e instanceof ListagemVagaSelecionadosComponent || ListagemVagasEmpresasComponent){
-      this.backgroundMenuListagemSelecionado = "background-branco";
-      this.strokeListagemSelecionado = "#41B8D2";
-    }
-    else if(e instanceof NotificacaoComponent){
-      this.backgroundMenuNotificacao = "background-branco";
-      this.strokeNotificacao ="#41B8D2";
-    }
+
+    if(e instanceof ListagemVagasComponent) this.ativarListagem()
+    else if(e instanceof MeuCadastroComponent) this.ativarMeusDados()
+    else if(e instanceof ListagemVagaSelecionadosComponent || ListagemVagasEmpresasComponent) this.ativarListagemSelecionada()
+    else if(e instanceof NotificacaoComponent) this.ativarNotificacao()
+
   }
 
   pintarMenu(e:any){
-    if(e.currentTarget.id == "usuario"){
 
-      this.backgroundMenuUser = "background-branco";
-      this.strokeUser="#41B8D2";
+    if(e.currentTarget.id == "usuario") this.ativarMeusDados()
+    else if(e.currentTarget.id == "listagem") this.ativarListagem()
+    else if(e.currentTarget.id == "listagem-selecionado") this.ativarListagemSelecionada()
+    else if (e.currentTarget.id == "notificacao") this.ativarNotificacao()
 
-      this.backgroundMenuListagem = "background-azul";
-      this.strokeListagem = "white";
-
-      this.backgroundMenuListagemSelecionado = "background-azul";
-      this.strokeListagemSelecionado = "white";
-
-      this.backgroundMenuNotificacao = "background-azul";
-      this.strokeNotificacao = "white";
-    }
-    else if(e.currentTarget.id == "listagem"){
-
-      this.backgroundMenuUser = "background-azul";
-      this.strokeUser="white";
-
-      this.backgroundMenuListagem = "background-branco";
-      this.strokeListagem = "#41B8D2";
-
-      this.backgroundMenuListagemSelecionado = "background-azul";
-      this.strokeListagemSelecionado = "white";
-
-      this.backgroundMenuNotificacao = "background-azul";
-      this.strokeNotificacao = "white";
-
-    }
-    else if(e.currentTarget.id == "listagem-selecionado"){
-
-      this.backgroundMenuUser = "background-azul";
-      this.strokeUser="white";
-
-      this.backgroundMenuListagem = "background-azul";
-      this.strokeListagem = "white";
-
-      this.backgroundMenuListagemSelecionado = "background-branco";
-      this.strokeListagemSelecionado = "#41B8D2";
-
-      this.backgroundMenuNotificacao = "background-azul";
-      this.strokeNotificacao = "white";
-
-    }
-    else if (e.currentTarget.id == "notificacao"){
-
-      this.backgroundMenuUser = "background-azul";
-      this.strokeUser="white";
-
-      this.backgroundMenuListagem = "background-azul";
-      this.strokeListagem = "white";
-
-      this.backgroundMenuListagemSelecionado = "background-azul";
-      this.strokeListagemSelecionado = "white";
-
-      this.backgroundMenuNotificacao = "background-branco";
-      this.strokeNotificacao ="#41B8D2";
-
-    }
   }
 
-  verificarExistenciaUsuarioLogado(){
-    if(sessionStorage.getItem('usuarioLogado')){
+  //********************************************************
+  //           Manipulação controladores menu
+  //********************************************************
 
-    }else{
+  ativarMeusDados(){
+
+    this.backgroundMenuUser = "background-branco";
+    this.strokeUser="#41B8D2";
+
+    this.backgroundMenuListagem = "background-azul";
+    this.strokeListagem = "white";
+
+    this.backgroundMenuListagemSelecionado = "background-azul";
+    this.strokeListagemSelecionado = "white";
+
+    this.backgroundMenuNotificacao = "background-azul";
+    this.strokeNotificacao = "white";
+
+  }
+
+  ativarListagem(){
+
+    this.backgroundMenuUser = "background-azul";
+    this.strokeUser="white";
+
+    this.backgroundMenuListagem = "background-branco";
+    this.strokeListagem = "#41B8D2";
+
+    this.backgroundMenuListagemSelecionado = "background-azul";
+    this.strokeListagemSelecionado = "white";
+
+    this.backgroundMenuNotificacao = "background-azul";
+    this.strokeNotificacao = "white";
+  }
+
+  ativarListagemSelecionada(){
+
+    this.backgroundMenuUser = "background-azul";
+    this.strokeUser="white";
+
+    this.backgroundMenuListagem = "background-azul";
+    this.strokeListagem = "white";
+
+    this.backgroundMenuListagemSelecionado = "background-branco";
+    this.strokeListagemSelecionado = "#41B8D2";
+
+    this.backgroundMenuNotificacao = "background-azul";
+    this.strokeNotificacao = "white";
+
+  }
+
+  ativarNotificacao(){
+
+    this.backgroundMenuUser = "background-azul";
+    this.strokeUser="white";
+
+    this.backgroundMenuListagem = "background-azul";
+    this.strokeListagem = "white";
+
+    this.backgroundMenuListagemSelecionado = "background-azul";
+    this.strokeListagemSelecionado = "white";
+
+    this.backgroundMenuNotificacao = "background-branco";
+    this.strokeNotificacao ="#41B8D2";
+
+  }
+
+  //********************************************************
+  //                 Login/Autenticação
+  //********************************************************
+
+  verificarExistenciaUsuarioLogado(){
+
+    if(!sessionStorage.getItem('usuarioLogado')){
       alert('Procedimento invalido')
       this.router.navigate(['/'])
     }
@@ -135,23 +142,25 @@ export class AppMainComponentComponent implements OnInit {
   }
 
   goToListagem(){
-    console.log(this.tipoUsuario?.tipo)
-    if(this.tipoUsuario?.tipo =="pessoa"){
-      this.router.navigate(["agencia-emprego/selecionados"])
-    }
-    else{
-      this.router.navigate(["agencia-emprego/vagas-empresa"])
-    }
+
+    if(this.tipoUsuario?.tipo =="pessoa") this.router.navigate(["agencia-emprego/selecionados"])
+    else this.router.navigate(["agencia-emprego/vagas-empresa"])
+
   }
 
   deslogar(){
+
     sessionStorage.removeItem('usuarioLogado')
+
   }
 
   pegarTipoUsuario(){
+
     if(sessionStorage.getItem('usuarioLogado')){
       let usuarioLogado = String(sessionStorage.getItem('usuarioLogado'))
       this.tipoUsuario = JSON.parse(usuarioLogado)
     }
+
   }
+
 }
